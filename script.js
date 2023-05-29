61a903dfbbdd5da3dc76f964c5482797eebb4f45
theo auffeuvre <theo.auffeuvre@gmail.com>
2023-05-29 10:27:08 +0200

    styling

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // import pdf from './node_modules/html-pdf';
 
 const getFlashcard = () => {
-  // const theme = document.querySelector(".flashcard-game-card-front h4").innerHTML;
-  // const question = document.querySelector(".flashcard-game-card-front .flashcard-game-card-content").innerHTML;
-  // const solution = document.querySelector(".flashcard-game-card-back .flashcard-game-card-content").innerHTML;
-  let theme = document.querySelector(".flashcard-game-card-title").innerHTML;
+  let theme = document.querySelector(".flashcard-game-card-header").innerHTML;
   let question = document.querySelector(".flashcard-game-card-content-markdown").innerHTML;
   let answer = document.querySelector(".flashcard-game-card-back .flashcard-game-card-content").innerHTML;
 
@@ -49,7 +46,60 @@ const updateCounter = () => {
 
 const showFlashcards = () => {
   // chrome.tabs.create({ url: "facebook.com" });
-  
+  let groupData = data.reduce((r, a) => {
+    r[a.theme] = [...r[a.theme] || [], a];
+    return r;
+  }, {});
+  console.log(Object.entries(groupData));
+  Object.entries(groupData).forEach((item) => {
+    console.log(item[0]);
+    let card = document.createElement('div');
+    card.classList.add('accordion-card');
+    let cardTitle = document.createElement('div');
+    cardTitle.classList.add('accordion-title');
+    cardTitle.innerHTML = item[0];
+    card.appendChild(cardTitle);
+    item[1].forEach((item2) => {
+      let cardContent = document.createElement('div');
+      cardContent.classList.add('accordion-content');
+      card.appendChild(cardContent);
+      let cardQuestion = document.createElement('p');
+      cardQuestion.classList.add('accordion-text');
+      cardQuestion.innerHTML = item2.question;
+      cardContent.appendChild(cardQuestion);
+      let cardAnswer = document.createElement('p');
+      cardAnswer.classList.add('accordion-text');
+      cardAnswer.innerHTML = item2.answer;
+      cardContent.appendChild(cardAnswer);
+    })
+    document.querySelector('#result').appendChild(card);
+  })
+
+  // groupData.map((item) => {
+  //   let card = document.createElement('div');
+  //   card.classList.add('card');
+  //   let cardTitle = document.createElement('div');
+  //   cardTitle.classList.add('card-title');
+  //   cardTitle.innerHTML = item.theme;
+  //   // let cardQuestion = document.createElement('p');
+  //   // cardQuestion.classList.add('card-text');
+  //   // cardQuestion.innerHTML = item.question;
+  //   card.appendChild(cardTitle);
+  //   document.querySelector('#result').appendChild(card);
+  // });
+}
+
+const isFlashcardPage = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    let tabUrl = tabs[0].url;
+    if (tabUrl.match(/kitt\.lewagon\.com\/camps\/\d*\/flashcards/gm)) {
+      document.querySelector('#add').disabled = false;
+    } else {
+      document.querySelector('#add').disabled = true;
+      document.querySelector('#add').title = "You must be on a flashcard page to add a flashcard";
+      document.querySelector('#add').style.cursor = "not-allowed";
+    }
+  });
 }
 
 if (!localStorage.getItem('data')) {
@@ -57,6 +107,7 @@ if (!localStorage.getItem('data')) {
 }
 let data = JSON.parse(localStorage.getItem('data'));
 updateCounter();
+isFlashcardPage();
 
 document.querySelector('#add').addEventListener("click", () => {
   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
